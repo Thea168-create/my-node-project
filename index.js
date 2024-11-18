e68afa00c1ff943762cc42d74001caab68b68225
Thea168-create <thea.t@tapeffect.asia>
2024-11-18 15:44:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, Node.js is working!") 
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, Node.js is working!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
